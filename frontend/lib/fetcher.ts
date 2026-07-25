d2d70b75fe8ead04bb9278ecaa68f230305302d3
merodev-coder <merodev1310@gmail.com>
2026-07-25 21:08:57 +0300

    a7a

--- frontend/lib/fetcher.ts
+++ frontend/lib/fetcher.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001'
+const API_URL = (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001').replace(/\/$/, '')
 
 export const fetcher = async (url: string) => {
   const res = await fetch(`${API_URL}${url}`, {
